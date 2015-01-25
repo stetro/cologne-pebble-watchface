9358904474c4be100d1332140e33d1cde272b6b1
Steffen Tröster <steffen.troester@googlemail.com>
2015-01-25 23:35:45 +0100

    addin

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
   
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 
@@ -26,6 +27,18 @@ static void update_time() {
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, buffer);
 }
+
+static void update_date() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+ 
+  // Create a long-lived buffer
+  static char buffer[] = "Sun 00";
+  strftime(buffer, sizeof("Sun 00"), "%a %e", tick_time);
+  // Display this time on the TextLayer
+  text_layer_set_text(s_date_layer, buffer);
+}
  
 static void main_window_load(Window *window) {
   // Create GBitmap, then set to created BitmapLayer
@@ -34,26 +47,37 @@ static void main_window_load(Window *window) {
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
   
-  // Create time TextLayer
+  // Create time TimeLayer
   s_time_layer = text_layer_create(GRect(35, 50, 144, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
+  
+  // Create time DateLayer
+  s_date_layer = text_layer_create(GRect(40, 75, 144, 50));
+  text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_color(s_date_layer, GColorBlack);
+  text_layer_set_text(s_date_layer, "Sun 00");
  
   // Improve the layout to be more like a watchface
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
- 
+  text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+  
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
   
   // Make sure the time is displayed from the start
   update_time();
+  update_date();
 }
  
 static void main_window_unload(Window *window) {
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_date_layer);
   
   // Destroy GBitmap
   gbitmap_destroy(s_background_bitmap);
@@ -64,6 +88,7 @@ static void main_window_unload(Window *window) {
  
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
+  update_date();
 }
   
 static void init() {
